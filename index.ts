c0c061459b102f588e7307592d7f6ec4a4d14183
Nguyen Thanh Nhan <nhannt.itech@gmail.com>
2021-12-06 08:56:16 +0700

    Heroku

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { errorHandler } from "./middleware";
 import adminRoutes from "./routes/admin";
-import morgan from "morgan";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -11,7 +10,6 @@ export const app = express();
 const port = process.env.PORT || 8000;
 
 import passport from "./middleware/passport/config";
-app.use(morgan("tiny"));
 app.use(express.json());
 app.use(passport.initialize());
 
